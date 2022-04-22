e270da59eff574a36ce7c5e55e1a088203b21cb3
juliokrause <jkq2000.fb@gmail.com>
2022-04-21 21:19:16 -0300

    Builded web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "033deccedee51ca4d91875b6e8b9ab53",
-"index.html": "6395d2713396adedd341d9834edec5d1",
-"/": "6395d2713396adedd341d9834edec5d1",
-"main.dart.js": "05590967c38b0272dc3cb50c16cb7646",
+"index.html": "429669da2fedbdb06413b00bc224cff2",
+"/": "429669da2fedbdb06413b00bc224cff2",
+"main.dart.js": "6332de254db90d925fe7e861f41e7243",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
